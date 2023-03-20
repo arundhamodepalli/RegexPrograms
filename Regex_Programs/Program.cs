64fbb3ed33e52f515066ceb97cbcde98835d071d
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-03-20 19:42:27 +0530

    Valid first time to regex programs

--- Regex_Programs/Program.cs
+++ Regex_Programs/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to regex programs");
+            // Console.WriteLine("welcome to regex programs");
+            ValidFirstName.Valid_FirstName();
         }
     }
 }
---
+++ Regex_Programs/ValidFirstName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Programs
+{
+        public class ValidFirstName
+        {
+            public static string Regix_Rule = "^[A-Z a-z]{3}$";
+            public static int Valid_FirstName()
+            {
+                string FirstName;
+                Console.WriteLine("Enter your FirstName: ");
+                FirstName = Convert.ToString(Console.ReadLine());
+
+                if (char.IsUpper(FirstName[0]) && FirstName.Length >= 3)
+                {
+                    Console.WriteLine("Valid firstName!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid FirstName. Please enter a first name that starts with a capital letter and has at least 3 characters.");
+                }
+
+                //Console.ReadLine();
+                return default;
+            }
+        }
+}
